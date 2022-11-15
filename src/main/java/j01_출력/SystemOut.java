077ede3dffdd5bad76181dd5680806cc219bf3d9
seokmin8 <meddo@naver.com>
2022-11-15 12:21:59 +0900

    챕터1 출력

---
+++ src/main/java/j01_출력/SystemOut.java
@@ -0,0 +1,14 @@
+package j01_출력;
+
+public class SystemOut {
+    public static void main(String[] args) {
+        System.out.print("이름 : ");
+        System.out.println("황석민");
+        System.out.print("주소 : ");
+        System.out.println("부산 북구 구포동 ");
+        System.out.print("연락처 : ");
+        System.out.println("010-6688-8778");
+        System.out.print("성별 : ");
+        System.out.println("남성");
+    }
+}
---
+++ src/main/java/j01_출력/UserInfo.java
@@ -0,0 +1,14 @@
+package j01_출력;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "황석민님";
+        String address = "부산 북구 구포동";
+        String phone = "010-6688-8778";
+        String age = "3n";
+
+        System.out.println(name + "의 주소는 " + address + " 입니다");
+        System.out.println(name + "의 연락처는 " + phone +" 입니다");
+        System.out.println(name + "의 나이는 " + age +"살 입니다");
+    }
+}
